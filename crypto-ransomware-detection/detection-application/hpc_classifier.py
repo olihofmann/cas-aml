f667661397c3f3c79fe1d39851fbb80eec16dbff
Oliver Hofmann <oli_hofmann@outlook.com>
2022-04-03 16:50:41 +0200

    Save image and add idx to class

--- crypto-ransomware-detection/detection-application/hpc_classifier.py
+++ crypto-ransomware-detection/detection-application/hpc_classifier.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import scipy
-import torch
+import matplotlib.pyplot as plt
+import uuid
 
 from pyts.image import GramianAngularField
 from PIL import Image
@@ -10,9 +10,12 @@ from torchvision import transforms
 
 class HpcClassifier:
     DATASET_COLUMNS = ["time-interval", "counter-value", "event", "runtime", "percentage"]
+    IDX_TO_CLASS = {0: "ransomware", 1: "benign"}
 
     def __init__(self) -> None:
         self.transformer: GramianAngularField = GramianAngularField()
+        self.model: BaselineNetwork = BaselineNetwork(number_of_classes=2, image_size=50)
+        self.model.load("../checkpoints/BI-GAF/best-checkpoint.ckpt")
 
     def _transform_to_image(self, df_transform: pd.DataFrame) -> np.ndarray:
         flatten_time_series: np.ndarray = np.array([df_transform.values.flatten()])
@@ -25,9 +28,8 @@ class HpcClassifier:
         df_counter_value: pd.DataFrame = df[["counter-value"]].copy()
         transformed_time_series: np.ndarray = self._transform_to_image(df_counter_value)
         image_time_series = Image.fromarray(transformed_time_series[0], mode="RGB")
+        plt.imsave(f"{uuid.uuid4()}.png", image_time_series)
         
-        model: BaselineNetwork = BaselineNetwork(number_of_classes=2, image_size=50)
-        model.load("../checkpoints/BI-GAF/best-checkpoint.ckpt")
         image_tensor = transforms.ToTensor()(image_time_series).unsqueeze_(0)
-        prediction = model.predict(image_tensor)
-        print(prediction.item())
+        prediction = self.model.predict(image_tensor)
+        print(self.IDX_TO_CLASS[prediction.item()])
