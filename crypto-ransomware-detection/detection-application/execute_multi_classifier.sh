d22ae8e6d6ec715c286218a71b9f2884b96a24d7
Oliver Hofmann <oli_hofmann@outlook.com>
2022-04-26 19:07:56 +0200

    Add execute script for multi detection app

---
+++ crypto-ransomware-detection/detection-application/execute_multi_classifier.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python ./multi/main.py --csv_file1_path ../data/benign-branch-instructions.csv --csv_file2_path ../data/benign-cache-references.csv --model_path ../checkpoints/MULTI-GAF/best-checkpoint-v3.ckpt
